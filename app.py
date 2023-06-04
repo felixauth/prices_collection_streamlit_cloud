eaacec4abde5889bfb75e3d1de3bccc0be21aaeb
felixauth <111569817+felixauth@users.noreply.github.com>
2023-06-04 16:31:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if launch_button:
     options = Options()
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
-    options.add_argument("--disable-blink-features=AutomationControlled") 
+#     options.add_argument("--disable-blink-features=AutomationControlled") 
 
     driver = get_driver()
 #     driver.get('http://www.jpg.fr')
